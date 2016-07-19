971cf130ee57d080a8f1d69e8483d483eaa6276a
d-theus <slma0x02@gmail.com>
2016-07-19 19:34:11 +0300

    fixed works factory

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -2,7 +2,13 @@ FactoryGirl.define do
   factory :work do
     title "MyString"
     description "MyString"
-    link "MyString"
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'vendor', 'assets', 'images', 'placeholder.png'))}
+    website_link "MyString"
+    github_link "MyString"
+
+    image { Struct.new(:url, :thumb_url, :small_url, :large_url) }
+
+    trait :with_image do
+      image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'vendor', 'assets', 'images', 'placeholder.png'))}
+    end
   end
 end
